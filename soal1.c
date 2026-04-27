dbd64808e24f50e48e9cad768b2b56c8fb858e76
Crowsten <Jonathanchandra2109@gmail.com>
2026-04-27 09:14:10 +0700

    Refactor soal1.c for improved input handling

--- soal1.c
+++ soal1.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int N;
 typedef struct Data {
     int *angin;
     int count;
-    int sorted;
     int median;
 } Data;
 
@@ -26,38 +23,39 @@ void bubbleSort(int arr[], int n)
     }
 }
 
+int main() {
+    Data data;
+    data.count = 0;
+    int size = 8;
+    data.angin = malloc(size * sizeof(int));
+    if (data.angin == NULL) return 1;
 
-int count = -1;
-int sorted = 0;
-int median;
-int temp=0;
-
-int main () {
-Data data;
-    for (int i=0;i<100;i++){
-        while (data.angin[i] != -1){
-            data.angin = (int *)malloc(N * sizeof(int));
-            scanf("%d", &data.angin[i]);
-            printf("INPUT %d\n", data.angin[i]);
-            count ++;
+    int x;
+    while (scanf("%d", &x) == 1) {
+        if (x == -1) break;
+        if (data.count >= size) {
+            size *= 2;
+            int *tmp = realloc(data.angin, size * sizeof(int));
+            if (tmp == NULL) break;
+            data.angin = tmp;
         }
-        break;
+        data.angin[data.count++] = x;
     }
-    printf("COUNT %d", count);
 
-    
+    printf("COUNT: %d", data.count);
 
-    bubbleSort(data.angin, count);
+    bubbleSort(data.angin, data.count);
     printf("SORTED ");
-    for (int i=0;i<count;i++){
+    for (int i = 0; i < data.count; i++) {
         printf("%d ", data.angin[i]);
     }
-
-    if (count % 2 == 0){
-        median = (*(data.angin + count/2) + *(data.angin + count/2 - 1)) / 2;
+    
+    if (data.count % 2 == 0) {
+        data.median = (data.angin[data.count/2] + data.angin[data.count/2 - 1]) / 2;
     } else {
-        median = *(data.angin + count/2);
+        data.median = data.angin[data.count/2];
     }
-    printf("MEDIAN %d\n", median);
-
+    printf("MEDIAN %d\n", data.median);
+    free(data.angin);
+    return 0;
 }
